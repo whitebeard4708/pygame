8afff6d44cb58d44faabc34e2284236f88cdca99
whitebeard4708 <38247356+whitebeard4708@users.noreply.github.com>
2018-10-24 19:58:40 +0800

    Update memorypuzzle_ver2.py

--- memorypuzzle_ver2.py
+++ memorypuzzle_ver2.py
@@ -1,4 +1,5 @@
 import pygame, sys
+import random
 from pygame.locals import *
 
 # color
@@ -10,7 +11,7 @@ PURPLE   = (255,   0, 255)
 BLACK    = (  0,   0,   0)
 color_set = (WHITE, RED, GREEN, NAVYBLUE, PURPLE, BLACK)
 BLUE1    = (10 , 10 , 100)
-YELLOW1  = (250, 210, 150)
+YELLOW1  = (250, 215, 160)
 WHITE1   = (230, 230, 220)
 
 # figure
@@ -26,9 +27,10 @@ figure_set = (RING, CROSS, SQUARE, CIRCLE, TRIANGLE, DIAMOND)
 board_width  = 6
 board_height = 4
 cover_color = BLUE1
-gapsize = 10
-boxsize = 50
+gapsize = 20
+boxsize = 60
 box_color = WHITE1
+assert (board_width * board_height) % 2 == 0, 'There is a odd number of boxes.'
 
 # interface size
 x_margin = int(boxsize * 1.5)
@@ -38,16 +40,73 @@ height  = 2 * y_margin + board_height * (boxsize + gapsize)
 background_color = YELLOW1
 FPS = 30
 reveal_speed = 8
-assert len(color_set)*len(figure_set) >= board_width * board_height, ...
+assert len(color_set)*len(figure_set) >= (board_width * board_height) / 2, ...
 'Not enough figures to be put into boxes!'
 
-pygame.init()
-DISPLAYSURF = pygame.display.set_mode((width, height))
-DISPLAYSURF.fill(background_color)
 
-while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-    pygame.display.update()
+
+def main():
+    global fpsClock, DISPLAYSURF
+    pygame.init()
+
+    fpsClock = pygame.time.Clock()
+    DISPLAYSURF = pygame.display.set_mode((width, height))
+    DISPLAYSURF.fill(background_color)
+    pygame.display.set_caption('Memory Puzzle')
+
+    # mainboard = getRandomizedBoard()
+
+    while True:
+        generateBoard()
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        pygame.display.update()
+
+
+def generateBoard():
+    boxx = x_margin + gapsize/2
+    for row in range(board_width):
+        boxy = y_margin + gapsize/2
+        for col in range(board_height):
+            pygame.draw.rect(DISPLAYSURF, box_color, (boxx, boxy, boxsize, boxsize))
+            boxy += (boxsize + gapsize)
+        boxx += (boxsize + gapsize)
+
+
+def getRandomizedBoard():
+    # create set of diffent figures with colors
+    icons = []
+    for color in color_set:
+        for figure in figure_set:
+            icons.append((color, figure))
+
+    # randomly arrange the set
+    random.shuffle(icons)
+    num_of_box = int(board_width * board_height) / 2
+    icons = icons[:num_of_box] * 2
+    random.shuffle(icons)
+
+    puzzle_board = []
+    add = 0
+    while add + board_width < num_of_box * 2:
+        row = icons[add:add+board_width]
+        puzzle_board.append(row)
+    return puzzle_board
+
+
+
+
+def drawBoard(board):
+    boxy = y_margin + gapsize/2
+    for row in range(board_height):
+        boxy = x_margin + gapsize/2
+        for col in range(board_width):
+            pygame.draw.rect(DISPLAYSURF, box_color, (boxx, boxy, boxsize, boxsize))
+
+            boxx += (boxsize + gapsize)
+        boxy += (boxsize + gapsize)
+
+if __name__ == '__main__':
+    main()
